948757112b32091aa7242e3fbf093f044aae4ea2
菜鸟阿凡 <103840107+yfabc123@users.noreply.github.com>
2023-10-14 23:49:35 +0800

    Add files via upload

--- 7_c++/9_CPP_Constructors_And_Destructors/4b.cpp
+++ 7_c++/9_CPP_Constructors_And_Destructors/4b.cpp
@@ -8,61 +8,63 @@ public:
     String(const char *str = nullptr);
     void show(void);
     void myFree();
+    ~String();
 
-private:
-    char *m_str = nullptr;
+    // private:
+    char *str = nullptr;
+    char *a;
 };
+String::~String()
+{
+
+    cout << "析构" << endl;
+    if (str)
+    {
+        delete[] str;
+    }
+}
 void String::myFree()
 {
-    cout << "调用myfree函数释放" << m_str << endl;
+    // cout << "myfree" << str << endl;
     // cout.clear();
-    //  printf("调用析构函数释放%p\n", m_str);
-
-    if (m_str)
-    {
-        delete[] m_str;
-    }
+    //  printf("调用析构函数释放%p\n", str);
 }
 String::String(const char *str)
 {
-    cout << "调用构造" << endl;
+    cout << "普通构造" << endl;
     if (str)
     {
 
         size_t len = strlen(str) + 1;
-        this->m_str = new char[len];
+        this->str = new char[len];
+        this->a = new char[len];
+        strcpy(this->str, str);
 
-        strcpy(this->m_str, str);
-
-        show();
+        //  show();
     }
     else
     {
-        this->m_str = NULL;
-        cout << "没有输入 无法调用show函数" << endl;
+        this->str = NULL;
     }
 }
 void String::show(void)
 {
 
-    cout << "测试" << endl;
-    for (size_t i = 0; i < strlen(m_str); i++)
+    for (size_t i = 0; i < strlen(str); i++)
     {
 
-        cout << m_str[i] << " ascii:" << endl;
+        cout << str[i] << " ascii:" << endl;
     }
 }
 
 int main()
 {
-    String a;
-    a.myFree();
+    String a("1");
     cout << "------------------------" << endl;
-    String *c = new String("abcd");
-    c->myFree();
+    String *c = new String("2");
+    delete c;
     cout << "------------------------" << endl;
-    String b("yefan");
-    b.myFree();
-
+    String b("3");
+    String d("4");
     return 0;
 }
